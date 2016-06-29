192fe497a54ccbec5c136cadc1ba7bc24a6959d4
America Calleros <america.quintero@gmail.com>
2016-06-29 14:09:42 -0500

    Test form data entry

--- spec/features/user_creates_person_spec.rb
+++ spec/features/user_creates_person_spec.rb
@@ -2,6 +2,13 @@ require "rails_helper"
 
 feature "user creates person" do
   scenario "with valid data" do
-    pending "Use Capybara to create a new person"
+      # Go to views/people/new
+    visit 'people/new'
+      # Fill out form field
+    fill_in 'First name', :with => 'Percey'
+      # Self explanitory
+    click_button "Create Person"
+      # Validate page text
+    expect(page).to have_content("Person created.")
   end
 end
